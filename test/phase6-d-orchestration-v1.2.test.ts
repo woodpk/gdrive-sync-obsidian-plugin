f89535fe5dc6121fe7f715775ec0daa7ab023365
woodpk <48619728+woodpk@users.noreply.github.com>
2026-08-31 22:31:30 -0400

    test(D): include production lifecycle composition acceptance

--- test/phase6-d-orchestration-v1.2.test.ts
+++ test/phase6-d-orchestration-v1.2.test.ts
@@ -1,3 +1,4 @@
 import "./workstreams/orchestration/v1.2-remote-feed-authority.test";
 import "./workstreams/orchestration/v1.2-authoritative-commit-lifecycle.test";
 import "./workstreams/orchestration/v1.2-folder-identity-production.test";
+import "./workstreams/orchestration/v1.2-production-lifecycle-composition.test";
