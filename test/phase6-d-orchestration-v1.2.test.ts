b13cfb38a149aca909b52855c0e85accdc6c48e8
woodpk <48619728+woodpk@users.noreply.github.com>
2026-08-31 23:17:06 -0400

    test(D-C10): discover planner-driven identity authority coverage

--- test/phase6-d-orchestration-v1.2.test.ts
+++ test/phase6-d-orchestration-v1.2.test.ts
@@ -2,3 +2,4 @@ import "./workstreams/orchestration/v1.2-remote-feed-authority.test";
 import "./workstreams/orchestration/v1.2-authoritative-commit-lifecycle.test";
 import "./workstreams/orchestration/v1.2-folder-identity-production.test";
 import "./workstreams/orchestration/v1.2-production-lifecycle-composition.test";
+import "./workstreams/orchestration/v1.2-production-identity-authority.test";
