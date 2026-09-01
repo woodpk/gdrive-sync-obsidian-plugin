7cc8425bc512b4a77636c8de1fe54e0b69cc0f79
woodpk <48619728+woodpk@users.noreply.github.com>
2026-08-31 22:28:28 -0400

    test(D): include production folder identity acceptance

--- test/phase6-d-orchestration-v1.2.test.ts
+++ test/phase6-d-orchestration-v1.2.test.ts
@@ -1,2 +1,3 @@
 import "./workstreams/orchestration/v1.2-remote-feed-authority.test";
 import "./workstreams/orchestration/v1.2-authoritative-commit-lifecycle.test";
+import "./workstreams/orchestration/v1.2-folder-identity-production.test";
